f2cf8968579e370012011f0103181720cec7d1f2
Gaby G <paogaby0803@gmail.com>
2020-05-14 18:47:44 -0500

    Updates

---
+++ precache-manifest.77fe5ba6b553eb237e7d085f3f50b449.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fccf0f27aa50e42991a7134b6020057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23032e28da351888fcf3",
+    "url": "/static/js/2.86b2f0d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86b2f0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2a1011740ccee6ee72",
+    "url": "/static/js/main.1c50cd1b.chunk.js"
+  },
+  {
+    "revision": "064d13c4de3942addb93",
+    "url": "/static/js/runtime-main.fbd4e595.js"
+  }
+]);
