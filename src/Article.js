fa9e96f9eba84d1821d220c566ed42fe0a316a78
Gaby G <paogaby0803@gmail.com>
2020-05-14 18:31:48 -0500

    add list post

---
+++ src/Article.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Article extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            article: this.props.article
+        }
+    }
+    render() {
+        return (
+
+            <div className="col-md-4">
+                <div className="blog-entry ">
+                    <div className="text text-2 pt-2 mt-3">
+                        <span className="category mb-3 d-block"><a href="#">Technology</a></span>
+                        <h3 className="mb-4"><a href="#">{this.state.article.title}</a></h3>
+                        <p className="mb-4">{this.state.article.body}
+                        </p>
+                        <div className="meta-wrap align-items-center">
+                            <div className="half order-md-last">
+                                <p className="meta">
+                                    <span><i className="icon-heart"></i>3</span>
+                                    <span><i className="icon-comment"></i>5</span>
+                                </p>
+                            </div>
+                            <div className="half">
+                                <p><a href="#" className="btn py-2">Continue Reading <span className="ion-ios-arrow-forward"></span></a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Article
--- src/Articles.js
+++ src/Articles.js
@@ -1,8 +1,17 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import Article from './Article'
+import dataArticle from './articles/articles.json'
 
-const Articles = props => {
-    const articles = props.articles.map(item =>{
-        return(
+class Articles extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            articles: dataArticle
+        }
+    }
+
+    render() {
+        return (
             <section className="ftco-section">
                 <div className="container">
                     <div className="row justify-content-center mb-5 pb-2">
@@ -13,35 +22,14 @@ const Articles = props => {
                         </div>
                     </div>
                     <div className="row">
-                    <div className="col-md-4">
-                        <div className="blog-entry ">
-                        <div className="text text-2 pt-2 mt-3">
-                            <span className="category mb-3 d-block"><a href="#">Technology</a></span>
-                            <h3 className="mb-4"><a href="#">{item.title}</a></h3>
-                            <p className="mb-4">{item.body}
-                            </p>
-                            <div className="meta-wrap align-items-center">
-                            <div className="half order-md-last">
-                                <p className="meta">
-                                <span><i className="icon-heart"></i>3</span>
-                                <span><i className="icon-comment"></i>5</span>
-                                </p>
-                            </div>
-                            <div className="half">
-                                <p><a href="#" className="btn py-2">Continue Reading <span className="ion-ios-arrow-forward"></span></a>
-                                </p>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
+                        {this.state.articles.map(article => {
+                            return <Article article={article} />
+                        })}
                     </div>
                 </div>
             </section>
-          )
-    })
-    
-    return <div>{articles}</div>
+        )
+    }
 }
 
 export default Articles
