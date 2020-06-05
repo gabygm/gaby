d53922ee4f2d72cc2418f3e0fb00d9a125ebbc0f
Gaby G <paogaby0803@gmail.com>
2020-06-04 21:51:49 -0500

    create route detail article

--- src/components/articles/ArticleHeader.js
+++ src/components/articles/ArticleHeader.js
@@ -1,38 +1,31 @@
-import React, { Component } from 'react';
-import ArticleDetail from './ArticleDetail';
+ import React, { Component } from 'react';
+ import ArticleDetail from './ArticleDetail';
+ import { useHistory } from 'react-router-dom';
 
-class Article extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            article: this.props.article
-        }
-        this.handleClick = this.handleClick.bind(this)
+export default (props) => {
+    const  article = props.article;
+    const history = useHistory();
 
+    function handleDetailArticlesClick(id){
+        history.push(`/article/${id}`)
     }
-    handleClick() {
-        return <ArticleDetail />
-    }
-    render() {
-        return (
-            <div className="col-md-12 article-home">
-                <div className="blog-entry">
-                    <div className="text text-2 pt-2 mt-3">
-                        <span className="category mb-3 d-block"><a href="#">Technology</a></span>
-                        <h3 className="mb-4"><a href="#">{this.state.article.title}</a></h3>
-                        <p className="mb-4">{this.state.article.body}
+
+    return (
+    <div className="col-md-12 article-home">
+        <div className="blog-entry">
+            <div className="text text-2 pt-2 mt-3">
+                <span className="category mb-3 d-block"><a href="#">Technology</a></span>
+                <h3 className="mb-4"><a href="#">{article.title}</a></h3> 
+                <p className="mb-4">{article.body}
+                 </p> 
+                <div className="meta-wrap align-items-center">
+                    <div className="half">
+                        <p><a href="#" className="btn py-2" onClick={handleDetailArticlesClick(article.id)} >Continue Reading <span className="ion-ios-arrow-forward"></span></a>
                         </p>
-                        <div className="meta-wrap align-items-center">
-                            <div className="half">
-                                <p><a href="#" className="btn py-2" onClick={this.handleClick} >Continue Reading <span className="ion-ios-arrow-forward"></span></a>
-                                </p>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    </div>
+    );
 }
-
-export default Article
--- src/router.js
+++ src/router.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './screens/home';
-
+import ArticleDetail from './screens/article_detail'
 
 export default () => (
     <Route
         render={() => (
             <Switch>
                 <Route path="/" component={HomePage} exact />
+                <Route path="/article/:id" component={ArticleDetail} exact />
             </Switch>
         )} />
 );
