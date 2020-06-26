bc2b26d8558b1d7d1d25281d5ed05a3f4f3c7273
Gaby G <paogaby0803@gmail.com>
2020-06-25 22:19:04 -0500

    Mange function

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import MenuSide from '../components/home/MenuSide'
-import Home from '../components/home/Home'
+import React, {useState} from 'react';
 
 export default () => {
+  function Button(pros){
+    return (
+      <button onClick={pros.handeClick}>
+       9
+      </button>
+    )
+  }
+
+  function Display(pros){
+    return(<div>{pros.counter}</div>)
+  }
+
+  function App(){
+    const [counter, setCounter]= useState(5)
+    const  handeClick = () => setCounter(counter+1)
+     return(
+       <div>
+         <Button handeClick={handeClick}/>
+         <Display counter={counter}/>
+       </div>
+     )
+  }
+
   return (
     <div id="colorlib-page">
-        <MenuSide />
-        <Home />
+      <App />
     </div>
   )
 }
