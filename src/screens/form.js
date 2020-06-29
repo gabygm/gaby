9fbad0e56912e751d784a7282b540d7e7ccd69ec
Gaby G <paogaby0803@gmail.com>
2020-06-28 21:08:45 -0500

    Form inputs

--- src/screens/form.js
+++ src/screens/form.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react'
 
 class Form extends Component {
+    state = { userName:''}
+    handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(this.state.userName)
+    }
     render() {
         return (
-            <form actionReact ="">
-                <input type='text' placeholder='Please enter your name' />
-                <button>Save Profile</button>
+            <form actionReact ="" onSubmit={this.handleSubmit}>
+                <input type='text' 
+                value={this.state.userName}
+                onChange={event=> this.setState({userName: event.target.value})}
+                placeholder='Please enter your name' />
+                <button >Save Profile</button>
             </form>
         )
     }
